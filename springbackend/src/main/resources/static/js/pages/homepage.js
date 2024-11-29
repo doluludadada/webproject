254508c7c54246c12aa94cc3d74fec4c9f40856c
doluludadada <doluludadada@outlook.com>
2024-11-29 17:33:41 +0800

    cc

--- springbackend/src/main/resources/static/js/pages/homepage.js
+++ springbackend/src/main/resources/static/js/pages/homepage.js
@@ -3,18 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var toggleNav = document.getElementById('toggleNav');
     if (toggleNav) {
         toggleNav.addEventListener('click', function() {
-            
             var navMenu = document.getElementById('navMenu');
             navMenu.classList.toggle('active');
-            // the content of the toggle button
-            this.textContent = navMenu.classList.contains('active') ? '×' : '☰';
+
+            // 取得按鈕中的圖標元素
+            var navIcon = this.querySelector('.nav-toggle-icon');
+            if (navIcon) {
+                // 切換圖標內容
+                navIcon.textContent = navMenu.classList.contains('active') ? '×' : '☰';
+            }
         });
     } else {
         console.error('Element with ID "toggleNav" was not found.');
     }
 });
-
-
-
-
-
